6f30e1d380f74ebd22047e2ce7ebf73580816548
Khai Nguyen <khainl1110@gmail.com>
2022-02-07 17:00:59 -0800

    Backend is set to connect with frontend

---
+++ src/main/java/com/example/backend/RestController/TestController.java
@@ -0,0 +1,17 @@
+package com.example.backend.RestController;
+
+import com.example.backend.Model.User;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class TestController {
+    
+    @GetMapping("/")
+    public User getMapping() {
+        return new User("khainl1110", "haha");
+    }
+}
