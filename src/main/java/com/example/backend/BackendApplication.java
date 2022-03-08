3098e6250ebd5d7cc8cbe5f7fa8a72bccd1d59dd
Matthew Chan <hohin.chan@sjsu.edu>
2022-03-07 20:46:24 -0800

    Created City model with haversine formula to calculate nearest City.

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,11 +3,15 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.backend.Model.City;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+//		City m = City.findNearest(35.514999, 138.756668);
+//		System.out.println(m.getCityName());
 	}
 
 }
