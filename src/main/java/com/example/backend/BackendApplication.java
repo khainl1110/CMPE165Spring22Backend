7d4af14571a1da12735780fce96a2bf940f077a9
Matthew Chan <hohin.chan@sjsu.edu>
2022-03-14 22:13:40 -0700

    Changed Customer to User

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -10,8 +10,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-//		City m = City.findNearest(35.514999, 138.756668);
-//		System.out.println(m.getCityName());
 	}
 
 }
--- src/main/java/com/example/backend/Model/User.java
+++ src/main/java/com/example/backend/Model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
-//    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+	
     private String username;
     private String password;
     private String email;
