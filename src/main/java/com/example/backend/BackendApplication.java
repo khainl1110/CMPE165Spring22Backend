d83b3471b3dc77c032e9fc878cbb2909786a4e4c
Matthew Chan <hohin.chan@sjsu.edu>
2022-03-14 22:23:00 -0700

    Fix merge conflicts

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -10,10 +10,7 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-<<<<<<< HEAD
-=======
 
->>>>>>> abba5a3257baa01201b85cb24a6cc7e98948737b
 	}
 
 }
