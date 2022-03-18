a39b501213c778a9935d240984f760c4fa52a812
Khuong Huynh <huynhkhuong69yahoo.com@Khangs-MacBook-Pro.local>
2022-03-17 23:16:42 -0700

    Add Cookie Methods in UserController

--- src/main/java/com/example/backend/RestController/UserController.java
+++ src/main/java/com/example/backend/RestController/UserController.java
@@ -4,6 +4,11 @@ import com.example.backend.Model.User;
 import com.example.backend.Repository.UserRepository;
 
 import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +17,47 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	//setting Http cookie
+	@GetMapping("/reservationCookie")
+	public String set_Cookie_Reservation(HttpServletResponse response) {
+  
+		//create a cookie
+		Cookie reserve = new Cookie("customerEmail", "sjsu@.edu");
+  
+		//set expiration day for cookie
+		reserve.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
+  
+		//Secure cookie, encrypted HTTPS
+		reserve.setSecure(true);
+  
+		//Http only, no JavaScript, to present cross-site attacks
+		reserve.setHttpOnly(true);
+  
+		//add cookie
+		response.addCookie(reserve);
+		return "Added customer Email!";
+	}
+  
+	//Reading cookie
+	@GetMapping("/allReservation")
+  
+	//HttpServletRequest method = read all cookie
+	//getCookies returns all cookies sent by client
+	public String read_Cookie_Reservation(HttpServletRequest request) {
+		Cookie[] reserve = request.getCookies();
+		if (reserve != null) {
+			return Arrays.stream(reserve).map(r -> r.getName() + r.getValue()).collect(Collectors.joining(", "));
+		}
+		return "Cookies!";
+	}
+ 
+
+
+
+
+
+
 	/*
 	
 	@Autowired
